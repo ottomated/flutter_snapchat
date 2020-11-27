16e13c0b0779ab1f31f89b2b97b21fc3e71da900
Jacob Brasil <jnxtbrasil@gmail.com>
2020-11-27 00:08:07 -0500

    Updated example imports

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_snapkit/snapkit.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:snapkit/snapkit.dart';
